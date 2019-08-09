263b4b4cb9573f2fde983469a1d883cd1416f58f
tankelvin <38410907+tankelvin@users.noreply.github.com>
2019-08-09 08:03:47 +0700

    Update article1.php

--- application/views/article1.php
+++ application/views/article1.php
@@ -18,15 +18,14 @@
 				<div class="row">
 					<div class="col-md-10 col-md-offset-1 text-center">
 						<ul class="hero-area-tree">
-							<li><a href="index.php">Home</a></li>
-							<li><a href="article.php">Article</a></li>
+							<li><a href="<?php echo site_url('Welcome/home');?>">Home</a></li>
+							<li><a href="<?php echo site_url('Welcome/article');?>">Article</a></li>
 							<li>Lulusan Terbaik, Putra Deddy Corbuzier Buktikan Kalau Anak “Broken Home” Juga Bisa Berprestasi</li>
 						</ul>
 						<h1 class="white-text">Lulusan Terbaik, Putra Deddy Corbuzier Buktikan Kalau Anak “Broken Home” Juga Bisa Berprestasi</h1>
 						<ul class="blog-post-meta">
-							<li class="blog-meta-author">By : <a href="#">Fhai</a></li>
+							<li class="blog-meta-author">By : Fhai</li>
 							<li>18 Oct, 2018</li>
-							<li class="blog-meta-comments"><a href="#"><i class="fa fa-comments"></i> 35</a></li>
 						</ul>
 					</div>
 				</div>
@@ -70,153 +69,11 @@
 						</div>
 						<!-- /blog share -->
 
-						<!-- blog comments -->
-						<div class="blog-comments">
-							<h3>5 Comments</h3>
-
-							<!-- single comment -->
-							<div class="media">
-								<div class="media-left">
-									<img src="./img/avatar.png" alt="">
-								</div>
-								<div class="media-body">
-									<h4 class="media-heading">John Doe</h4>
-									<p>Cu his iudico appareat ullamcorper, at mea ignota nostrum. Nonumy argumentum id cum, eos adversarium contentiones id</p>
-									<div class="date-reply"><span>Oct 18, 2017 - 4:00AM</span><a href="#" class="reply">Reply</a></div>
-								</div>
-
-								<!-- comment reply -->
-								<div class="media">
-									<div class="media-left">
-										<img src="./img/avatar.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4 class="media-heading">John Doe</h4>
-										<p>Cu his iudico appareat ullamcorper, at mea ignota nostrum. Nonumy argumentum id cum, eos adversarium contentiones id</p>
-										<div class="date-reply"><span>Oct 18, 2017 - 4:00AM</span><a href="#" class="reply">Reply</a></div>
-									</div>
-								</div>
-								<!-- /comment reply -->
-
-								<!-- comment reply -->
-								<div class="media">
-									<div class="media-left">
-										<img src="./img/avatar.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4 class="media-heading">John Doe</h4>
-										<p>Cu his iudico appareat ullamcorper, at mea ignota nostrum. Nonumy argumentum id cum, eos adversarium contentiones id</p>
-										<div class="date-reply"><span>Oct 18, 2017 - 4:00AM</span><a href="#" class="reply">Reply</a></div>
-									</div>
-								</div>
-								<!-- /comment reply -->
-
-							</div>
-							<!-- /single comment -->
-
-							<!-- single comment -->
-							<div class="media">
-								<div class="media-left">
-									<img src="./img/avatar.png" alt="">
-								</div>
-								<div class="media-body">
-									<h4 class="media-heading">John Doe</h4>
-									<p>Cu his iudico appareat ullamcorper, at mea ignota nostrum. Nonumy argumentum id cum, eos adversarium contentiones id</p>
-									<div class="date-reply"><span>Oct 18, 2017 - 4:00AM</span><a href="#" class="reply">Reply</a></div>
-								</div>
-							</div>
-							<!-- /single comment -->
-
-							<!-- blog reply form -->
-							<div class="blog-reply-form">
-								<h3>Leave Comment</h3>
-								<form>
-									<input class="input name-input" type="text" name="name" placeholder="Name">
-									<input class="input email-input" type="email" name="email" placeholder="Email">
-									<textarea class="input" name="message" placeholder="Enter your Message"></textarea>
-									<button class="main-button icon-button">Submit</button>
-								</form>
-							</div>
-							<!-- /blog reply form -->
-
-						</div>
-						<!-- /blog comments -->
 					</div>
 					<!-- /main blog -->
 
 					<!-- aside blog -->
 					<div id="aside" class="col-md-3">
-
-						<!-- search widget -->
-						<div class="widget search-widget">
-							<form>
-								<input class="input" type="text" name="search">
-								<button><i class="fa fa-search"></i></button>
-							</form>
-						</div>
-						<!-- /search widget -->
-
-						<!-- category widget -->
-						<div class="widget category-widget">
-							<h3>Categories</h3>
-							<a class="category" href="#">Web <span>12</span></a>
-							<a class="category" href="#">Css <span>5</span></a>
-							<a class="category" href="#">Wordpress <span>24</span></a>
-							<a class="category" href="#">Html <span>78</span></a>
-							<a class="category" href="#">Business <span>36</span></a>
-						</div>
-						<!-- /category widget -->
-
-						<!-- posts widget -->
-						<div class="widget posts-widget">
-							<h3>Recents Posts</h3>
-
-							<!-- single posts -->
-							<div class="single-post">
-								<a class="single-post-img" href="blog-post.html">
-									<img src="./img/post01.jpg" alt="">
-								</a>
-								<a href="blog-post.html">Pro eu error molestie deserunt.</a>
-								<p><small>By : John Doe .18 Oct, 2017</small></p>
-							</div>
-							<!-- /single posts -->
-
-							<!-- single posts -->
-							<div class="single-post">
-								<a class="single-post-img" href="blog-post.html">
-									<img src="./img/post02.jpg" alt="">
-								</a>
-								<a href="blog-post.html">Pro eu error molestie deserunt.</a>
-								<p><small>By : John Doe .18 Oct, 2017</small></p>
-							</div>
-							<!-- /single posts -->
-
-							<!-- single posts -->
-							<div class="single-post">
-								<a class="single-post-img" href="blog-post.html">
-									<img src="./img/post03.jpg" alt="">
-								</a>
-								<a href="blog-post.html">Pro eu error molestie deserunt.</a>
-								<p><small>By : John Doe .18 Oct, 2017</small></p>
-							</div>
-							<!-- /single posts -->
-
-						</div>
-						<!-- /posts widget -->
-
-						<!-- tags widget -->
-						<div class="widget tags-widget">
-							<h3>Tags</h3>
-							<a class="tag" href="#">Web</a>
-							<a class="tag" href="#">Photography</a>
-							<a class="tag" href="#">Css</a>
-							<a class="tag" href="#">Responsive</a>
-							<a class="tag" href="#">Wordpress</a>
-							<a class="tag" href="#">Html</a>
-							<a class="tag" href="#">Website</a>
-							<a class="tag" href="#">Business</a>
-						</div>
-						<!-- /tags widget -->
 
 					</div>
 					<!-- /aside blog -->
