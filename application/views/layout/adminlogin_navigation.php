867760a2f1756a2c1762263693b4732b4d13a9c3
tankelvin <38410907+tankelvin@users.noreply.github.com>
2019-08-09 09:07:31 +0700

    Admin

--- application/views/layout/adminlogin_navigation.php
+++ application/views/layout/adminlogin_navigation.php
@@ -5,7 +5,7 @@
 				<div class="navbar-header">
 					<!-- Logo -->
 					<div class="navbar-brand">
-						<a class="logo" href="<?php echo site_url('Welcome/home');?>">
+						<a class="logo" href="<?php echo site_url('Welcome/homeadmin');?>">
 							<img src="<?php echo base_url();?>assets/img/logo-alt.png" alt="logo">
 						</a>
 					</div>
@@ -21,10 +21,8 @@
 				<!-- Navigation -->
 				<nav id="nav">
 					<ul class="main-menu nav navbar-nav navbar-right">
-						<li><a href="<?php echo site_url('Welcome/home');?>">Home</a></li>
-						<li><a href="<?php echo site_url('Welcome/courses');?>">Course</a></li>
-						<li><a href="<?php echo site_url('Welcome/article');?>">Article</a></li>
-						<li><a href="<?php echo site_url('Welcome/contact');?>">Contact</a></li>
+						<li><a href="<?php echo site_url('Welcome/users');?>">Users</a></li>
+						<li><a href="<?php echo site_url('Welcome/comment');?>">Comment</a></li>
 						<li><a href="<?php echo site_url('Welcome/index');?>">Logout</a></li>
 					</ul>
 				</nav>
